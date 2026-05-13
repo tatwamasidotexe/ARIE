e74ad15e61057c0bbff9017f2b5338f834ce011c
Tatwamasi Mishra <tatwamasimishra@tatwamasim4.local>
2026-05-13 16:31:09 +0530

    added allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,7 +48,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ariebytatwamasi.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
